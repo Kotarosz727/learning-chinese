660a5af6869b27f517d34f3589536357ade54940
Kotarosz727 <yoga583sure246>
2020-12-28 15:01:28 +0900

    add icon in index

--- components/card.tsx
+++ components/card.tsx
@@ -1,26 +1,32 @@
 import React, { useState } from "react";
 import styles from "./card.module.css";
 import SpeakerIcon from "@material-ui/icons/Speaker";
+import AutorenewIcon from "@material-ui/icons/Autorenew";
+import CachedRoundedIcon from '@material-ui/icons/CachedRounded';
 
 export default function card({ sentence, index }) {
     const speak = (s) => {
         responsiveVoice.speak(s, "Chinese Female");
     };
     const [flip, setFlip] = useState(false);
-    const front = <div className={styles.front}><h2>{sentence.japanese}</h2></div>;
+
+    const front = (
+        <div className={styles.front}>
+            <h2>{sentence.japanese}</h2>
+        </div>
+    );
     const back = (
         <div className={styles.back}>
-            <SpeakerIcon color="action" style={{ fontSize: 35 }} onMouseEnter={() => speak(sentence.chinese)} />
-            <h2>{sentence.chinese}</h2> 
-            <p style={{fontSize: 14}}>{sentence.pinin}</p>
+            <SpeakerIcon color="action" style={{ fontSize: 35 }} onMouseDown={() => speak(sentence.chinese)} />
+            <h2>{sentence.chinese}</h2>
+            <p style={{ fontSize: 14 }}>{sentence.pinin}</p>
         </div>
     );
 
     return (
-        <div className={`${styles.card} ${flip ? styles.flip : ''}`} onMouseDown={() => setFlip(!flip)} onTouchStart={() => setFlip(!flip)}>
+        <div className={`${styles.card} ${flip ? styles.flip : ""}`}>
             {flip ? back : front}
-            {/* {front}
-            {back} */}
+            <span className={styles.button} onMouseDown={() => setFlip(!flip)}><CachedRoundedIcon fontSize="large"/></span>
         </div>
     );
 }
