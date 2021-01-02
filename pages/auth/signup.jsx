4b4b8e6b811927c261ca9592ae5686cebd230bf5
Kotarosz727 <yoga583sure246>
2021-01-02 23:41:53 +0900

    css revise on signup tsx

--- pages/auth/signup.jsx
+++ pages/auth/signup.jsx
@@ -5,6 +5,17 @@ export default function signup() {
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
 
+    const formStyle = {
+        display: 'block',
+        alignItems: 'center',
+        margin: '0 auto',
+        position: 'relative',
+        border: '2px solid #ccc',
+        borderRadius: '0.25rem',
+        width: '350px',
+        height: '200px',
+    };
+
     function handleName(e) {
         setName(e.target.value);
     }
@@ -15,20 +26,21 @@ export default function signup() {
     async function aws_signUP() {
         try {
             const { user } = await Auth.signUp({
-                'password': password,
-                'username': name,
-                'attributes': {
-                    'name': name,
+                password: password,
+                username: name,
+                attributes: {
+                    name: name,
                 },
             });
-            console.log('ok:', user);
+            console.log("ok:", user);
         } catch (error) {
             console.log("error signing up:", error);
         }
     }
 
     return (
-        <div>
+        <div style={formStyle}>
+            <span>Sign up</span>
             <input type="text" placeholder="username" onChange={handleName} />
             <input type="text" placeholder="password" onChange={handlePassword} />
             <button onClick={aws_signUP}>Sign Up</button>
