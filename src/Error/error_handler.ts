136d5b3b4e5ff88cf97b21ef554de51a6e8966c2
Kotarosz727 <yoga583sure246>
2021-01-04 20:33:29 +0900

    add error_handle class

---
+++ src/Error/error_handler.ts
@@ -0,0 +1,28 @@
+interface error_obj {
+    kind: string;
+    msg: string;
+}
+export default class error_handler {
+    res: error_obj
+
+    public handle_error = (e: any): error_obj => {
+        if (e == "nonameException") {
+            let res = { kind: "name", msg: "ユーザー名を入力してください" };
+            return res;
+        } else if (e == "noPasswordException") {
+            let res = { kind: "password", msg: "パスワードを入力してください" };
+            return res;
+        } else if (e == "shortPasswordException") {
+            let res = { kind: "password", msg: "パスワードは8文字以上を入力してください" };
+            return res;
+        } else if (e.name == "UsernameExistsException") {
+            let res = { kind: "name", msg: "ユーザー名はすでに登録されています" };
+            return res;
+        } else if (e.message == "Password did not conform with policy: Password must have numeric characters") {
+            let res = { kind: "password", msg: "パスワードには数字を含めてください" };
+            return res;
+        } else {
+            throw e;
+        }
+    } 
+}
