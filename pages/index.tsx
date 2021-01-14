2e9f5d4f878ebe365adb7c8eccc2526d5a905d58
Kotarosz727 <yoga583sure246>
2021-01-14 12:45:33 +0900

    switch boormark icon if bokkmarked

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,24 +8,59 @@ import ChineseInterator from "../src/interactors/Chinese/ChineseInterator";
 import BlockIcon from "@material-ui/icons/Block";
 import Pagination from "../components/pagination";
 
-export default function component({ sentence, url }) {
+export default function component({ sentence, url_main, url_favorite }) {
+    type type_favarites = {
+        userid: string;
+        chinese: string;
+        pinin: string;
+        japanese: string;
+    }[];
+    const userid = useContext(UserContext);
+    // const [favorites, setFavorites] = useState<type_favarites | []>([]);
+
+    let s = sentence;
+
+    useEffect(() => {
+        const fetchFavorite = async (url_favorite, userid) => {
+            const res: type_favarites = await new ChineseInterator().fetchFavorites(url_favorite, userid);
+            if (res.length) {
+                const bookmark_array = [];
+                res?.map((r) => {
+                    bookmark_array.push(r.chinese);
+                });
+                console.log("aaaaaa", bookmark_array);
+                s.map((v) => {
+                    if (bookmark_array.findIndex((item) => item === v.chinese) >= 0) {
+                        v.bookmark = true
+                    }
+                });
+            }
+        };
+        if (userid) {
+            fetchFavorite(url_favorite, userid);
+        }
+    }, userid);
+
     return (
         <>
             <Head title="中国语学习"></Head>
             <div className={styles.container}>
-                <Pagination sentence={sentence} url={url} />
+                <Pagination sentence={s} url={url_main} />
             </div>
         </>
     );
 }
 
 export const getStaticProps = async (): Promise<object> => {
-    const url = process.env.LAMBDA_URL;
-    const sentence = await new ChineseInterator().fetchLists(url) ?? [];
+    const url_main = process.env.LAMBDA_URL;
+    const url_favorite = process.env.LAMBDA_URL2;
+
+    const sentence = (await new ChineseInterator().fetchLists(url_main)) ?? [];
     return {
         props: {
             sentence,
-            url,
+            url_main,
+            url_favorite,
         },
     };
 };
