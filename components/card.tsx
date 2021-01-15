dd1fcb588e413698cffe77b8f3fc227bdfb10513
Kotarosz727 <yoga583sure246>
2021-01-15 18:11:28 +0900

    refactoring card.tsx

--- components/card.tsx
+++ components/card.tsx
@@ -2,12 +2,9 @@ import React, { useState, useContext, useEffect } from "react";
 import styles from "./card.module.css";
 import SpeakerIcon from "@material-ui/icons/Speaker";
 import CachedRoundedIcon from "@material-ui/icons/CachedRounded";
-import { AuthState } from "@aws-amplify/ui-components";
 import BookmarkBorderOutlinedIcon from "@material-ui/icons/BookmarkBorderOutlined";
-import { setupMaster } from "cluster";
 import ChineseInterator from "../src/interactors/Chinese/ChineseInterator";
 import BookmarkIcon from "@material-ui/icons/Bookmark";
-import BookMark from "../pages/bookmark";
 
 export default function card({ sentence, index, userid, url }): JSX.Element {
     type type_favarites = {
@@ -17,28 +14,45 @@ export default function card({ sentence, index, userid, url }): JSX.Element {
         japanese: string;
     }[];
 
-    const [s, sets] = useState(sentence);
-
-    const speak = (s) => {
-        responsiveVoice.speak(s, "Chinese Female");
+    const [content, setContent] = useState(sentence);
+    const [flip, setFlip] = useState(false);
+    const speak = (value) => {
+        responsiveVoice.speak(value, "Chinese Female");
     };
     const postFavorite = async () => {
         await new ChineseInterator().postFavorite(sentence, userid, url);
     };
+    const front = (
+        <div className={styles.front}>
+            <h2>{sentence.japanese}</h2>
+        </div>
+    );
+    const back = (
+        <div className={styles.back}>
+            <SpeakerIcon
+                color="action"
+                style={{ fontSize: 35 }}
+                onTouchStart={() => speak(sentence.chinese)}
+                onMouseDown={() => speak(sentence.chinese)}
+            />
+            <h2>{sentence.chinese}</h2>
+            <p style={{ fontSize: 20 }}>{sentence.pinin}</p>
+        </div>
+    );
 
     useEffect(() => {
         const fetchFavorite = async (url_favorite, userid) => {
             const res: type_favarites = await new ChineseInterator().fetchFavorites(url_favorite, userid);
             if (res?.length) {
-                const bookmark_array = [];
+                const bookmarked: string[] = [];
                 res?.map((r) => {
-                    bookmark_array.push(r.chinese);
+                    bookmarked.push(r.chinese);
                 });
-                if (bookmark_array.findIndex((item) => item === s.chinese) >= 0){
-                    s.bookmark = true
+                if (bookmarked.findIndex((item) => item === content.chinese) >= 0) {
+                    content.bookmark = true;
                 }
-                const updated = {...s}
-                sets(updated)  
+                const updated = { ...content };
+                setContent(updated);
             }
         };
         if (userid) {
@@ -61,26 +75,6 @@ export default function card({ sentence, index, userid, url }): JSX.Element {
             </span>
         );
     }
-
-    const [flip, setFlip] = useState(false);
-
-    const front = (
-        <div className={styles.front}>
-            <h2>{sentence.japanese}</h2>
-        </div>
-    );
-    const back = (
-        <div className={styles.back}>
-            <SpeakerIcon
-                color="action"
-                style={{ fontSize: 35 }}
-                onTouchStart={() => speak(sentence.chinese)}
-                onMouseDown={() => speak(sentence.chinese)}
-            />
-            <h2>{sentence.chinese}</h2>
-            <p style={{ fontSize: 20 }}>{sentence.pinin}</p>
-        </div>
-    );
 
     return (
         <div className={`${styles.card} ${flip ? styles.flip : ""}`} key={index}>
